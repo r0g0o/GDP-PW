ebe429801d811fd57afb3e8e1879e9ba4065ab04
Rodrigo Castillo <rcastillo159@gmail.com>
2021-02-08 15:39:11 -0300

    8-2-21

--- Script.js
+++ Script.js
@@ -1,50 +1,39 @@
-function mostrarocultar(x) {
-    var formato = document.getElementById(x)
-    var elemento = document.getElementById(x)
-    if (formato.style.display == "block") {
-        elemento.style.display = "none";
-    }
-    else {
-        elemento.style.display = "block";
-    }
+function moverse() {
+    window.scrollBy(0,-35)
 }
 
 function mostrartodouno() {
-    document.getElementById('unouno').style.display = "block";
-    document.getElementById('unodos').style.display = "block";
-    document.getElementById('unotres').style.display = "block";
-    document.getElementById('unocuatro').style.display = "block";
-    document.getElementById('unocinco').style.display = "block";
-    document.getElementById('unoseis').style.display = "block";
-    document.getElementById('unosiete').style.display = "block";
+    document.getElementById('11').open = true;
+    document.getElementById('12').open = true;
+    document.getElementById('13').open = true;
+    document.getElementById('14').open = true;
+    document.getElementById('15').open = true;
+    document.getElementById('16').open = true;
 }
 
 function ocultartodouno() {
-    document.getElementById('unouno').style.display = "none";
-    document.getElementById('unodos').style.display = "none";
-    document.getElementById('unotres').style.display = "none";
-    document.getElementById('unocuatro').style.display = "none";
-    document.getElementById('unocinco').style.display = "none";
-    document.getElementById('unoseis').style.display = "none";
-    document.getElementById('unosiete').style.display = "none";
+    document.getElementById('11').open = false;
+    document.getElementById('12').open = false;
+    document.getElementById('13').open = false;
+    document.getElementById('14').open = false;
+    document.getElementById('15').open = false;
+    document.getElementById('16').open = false;
 }
 
 function mostrartododos() {
-    document.getElementById('dosuno').style.display = "block";
-    document.getElementById('dosdos').style.display = "block";
-    document.getElementById('dostres').style.display = "block";
-    document.getElementById('doscuatro').style.display = "block";
-    document.getElementById('doscinco').style.display = "block";
-    document.getElementById('dosseis').style.display = "block";
-    document.getElementById('dossiete').style.display = "block";
+    document.getElementById('21').open = true;
+    document.getElementById('22').open = true;
+    document.getElementById('23').open = true;
+    document.getElementById('24').open = true;
+    document.getElementById('25').open = true;
+    document.getElementById('26').open = true;
 }
 
 function ocultartododos() {
-    document.getElementById('dosuno').style.display = "none";
-    document.getElementById('dosdos').style.display = "none";
-    document.getElementById('dostres').style.display = "none";
-    document.getElementById('doscuatro').style.display = "none";
-    document.getElementById('doscinco').style.display = "none";
-    document.getElementById('dosseis').style.display = "none";
-    document.getElementById('dossiete').style.display = "none";
+    document.getElementById('21').open = false;
+    document.getElementById('22').open = false;
+    document.getElementById('23').open = false;
+    document.getElementById('24').open = false;
+    document.getElementById('25').open = false;
+    document.getElementById('26').open = false;
 }
